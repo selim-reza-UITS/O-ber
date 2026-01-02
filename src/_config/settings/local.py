f3011286664116c6874866c8e584f5c3edd8cba0
selim-reza-UITS <selim.reza.uits@gmail.com>
2026-01-02 17:55:42 +0600

    Conf: DRF settings added

--- src/_config/settings/local.py
+++ src/_config/settings/local.py
@@ -1,1 +1,8 @@
 from src._config.settings.base import *  # noqa: F403
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
